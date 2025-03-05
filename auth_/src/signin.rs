3770fcf97b809c18b5a3878b1314cb6460c8d419
i18n <i18n.site@gmail.com>
2025-03-05 13:49:27 +0800

    ♨

--- auth_/src/signin.rs
+++ auth_/src/signin.rs
@@ -1,9 +1,17 @@
 use header_host::header_host;
-use set_cookie::SET_COOKIE;
 use aok::{OK, Result};
+use xkv::{
+  R,
+  fred::interfaces::{KeysInterface, SortedSetsInterface},
+};
+use set_cookie::{MAX, SET_COOKIE};
 use http::{Extensions, HeaderMap};
 use ctx_::SetHeader;
 use cookie_b::Browser;
+use xbin::concat;
+
+pub const R_BROWSER: &[u8] = b"B:";
+pub const R_USER_BROWSER: &[u8] = b"B:";
 
 #[iat::captcha]
 pub async fn mail(
@@ -13,11 +21,45 @@ pub async fn mail(
   set_header: &SetHeader,
   browser: &Browser,
 ) -> Result<()> {
+  /*
+    根据域名id，邮箱确认用户，不同网站可以复用一套后台
+  */
+
   let host = header_host(headers)?;
   let cookie = set_cookie::new(xtld::host_tld(host));
   let uid = 12;
 
-  dbg!(browser);
+  let now = sts::sec() as f64;
+
+  let uid_bin = &intbin::u64_bin(uid)[..];
+
+  let key = concat!(R_BROWSER, browser.bin);
+
+  /*
+
+  browser -> user 浏览器登录了的用户
+  user -> browser 用户在哪些设备上登录
+
+  */
+  let p = R.pipeline();
+  let _: () = p
+    .zadd(key, None, None, false, false, (now, uid_bin))
+    .await?;
+  let _: () = p.expire(key, MAX as _, None).await?;
+  let _: () = p
+    .zadd(
+      concat!(R_USER_BROWSER, uid_bin),
+      None,
+      None,
+      false,
+      false,
+      (now, &browser.bin[..]),
+    )
+    .await?;
+
+  dbg!(headers);
+  let _: () = p.last().await?;
+
   set_header.push(SET_COOKIE, cookie.set_max_for_js("u", ub64::u64_b64(uid)));
   OK
 }
