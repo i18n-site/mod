2385be055f455970f367c783b4090300d53c90ef
i18n <i18n.site@gmail.com>
2025-03-06 21:58:58 +0800

    ♨

--- auth_/cron/main.js
+++ auth_/cron/main.js
@@ -1,11 +1,10 @@
 import bantld from "@8v/bantld";
 import heartbeat from "@8v/heartbeat/kind.js";
-import nextMinute from "@8v/cron/nextMinute.js";
 
 const KIND = "bantld";
 
 Deno.cron(
   KIND, 
-  nextMinute(), 
+  "0 22 * * *", 
   heartbeat(KIND, bantld, 86400 * 5)
 );
