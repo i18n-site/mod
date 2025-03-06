4a49c5622aaa165c06ab46ef7ed4aa0ac48b8bfb
i18n <i18n.site@gmail.com>
2025-03-06 21:49:21 +0800

    ♨

--- auth_/cron/main.js
+++ auth_/cron/main.js
@@ -1,1 +1,8 @@
-import '@8v/bantld'
+import bantld from "@8v/bantld.js";
+import heartbeat from "@8v/heartbeat/kind.js";
+import cron from "@8v/cron";
+import nextMinute from "@8v/cron/nextMinute.js";
+
+KIND = "bantld";
+
+cron(KIND, nextMinute(), heartbeat(KIND, bantld, 86400 * 5));
